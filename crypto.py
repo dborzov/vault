f4a1c4de8006125be2c114b9917413285c4c7318
Peter Borzov <peter@sowingo.com>
2014-05-10 21:47:43 -0400

    HMAC?

--- crypto.py
+++ crypto.py
@@ -1,1 +1,5 @@
 hashlib.sha1(fh.read()).hexdigest() #sha1 things!
+
+
+string_to_encode = hmac.new(str(secret), str("superstring!"), hashlib.sha1).digest()
+secret =  base64.b64encode(string_to_encode)
